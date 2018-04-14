b6f4c054c75b05ab80e7ef1a121bc41209260608
Angeles <adaldunate@miuandes.cl>
2018-04-14 16:47:38 -0300

    added first view

--- imageapp/models.py
+++ imageapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class ExampleModel(models.Model):
+    model_pic = models.ImageField(upload_to = 'pic_folder/', default = 'pic_folder/None/no-img.jpg')
--- imageapp/views.py
+++ imageapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from clarifai.rest import ClarifaiApp
 from clarifai.rest import Image as ClImage
+from django.template import loader
 
 
 # Create your views here.
@@ -8,6 +9,7 @@ from django.http import HttpResponse
 
 
 def index(request):
+
     app = ClarifaiApp(api_key='f0e9f06f4bab4b0f918522fac94b2eb4')
 
     model = app.models.get('general-v1.3')
@@ -16,7 +18,21 @@ def index(request):
     image = ClImage(url='https://samples.clarifai.com/metro-north.jpg')
     res = model.predict([image])
     print(res)
+    context = {
+        'res': res,
+    }
+
+    template = loader.get_template('imageapp/index.html')
+
+    return HttpResponse(template.render(context, request))
 
 
-
-    return HttpResponse(str(res))
+'''def upload_pic(request):
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            m = ExampleModel.objects.get(pk=course_id)
+            m.model_pic = form.cleaned_data['image']
+            m.save()
+            return HttpResponse('image upload success')
+    return HttpResponseForbidden('allowed only via POST')'''

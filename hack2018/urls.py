002f3d551c8f28ede66aed1313612e270144ba16
Alfonso <ajzuniga1@miuandes.cl>
2018-04-14 18:38:03 -0300

    Worked on index html file

--- hack2018/urls.py
+++ hack2018/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('imageapp.urls')),
     path('imageapp/', include('imageapp.urls')),
     path('admin/', admin.site.urls),
 ]
